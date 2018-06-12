6cad2b16f92bc8d2b73ecb9596a332970340c7f3
Berry  Semexan <bsemexan@gmail.com>
2018-06-12 03:23:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
 
   def upload
     CSV.foreach(params[:file].path, headers: true) do |song|
-      Artist.create(title: song[0], artist_name: song[1])
+      Song.create(title: song[0], artist_name: song[1])
     end
     redirect_to songs_path
   end
